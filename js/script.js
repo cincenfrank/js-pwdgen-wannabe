83793e812fca191f35be8015d46a7f653ddc8ade
cincenfrank <fpetrangeli1@gmail.com>
2021-10-07 15:28:44 +0200

    added secret debug log

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
+console.log(
+  "new wannabe victim found. Asking name in order to use it in next prompt"
+);
 let firstName = prompt(`Welcome to our advanced Password Generator.
 If you want to obtain a fantastic password, type your name.`);
 
+console.log("waiting to get last name in order to use it in next prompt");
 let lastName =
   prompt(`Well done ${firstName}, in order to be "hacker-proof", please type your last name.
 We are GDPR compliant, so, with us, your data are safe!`);
 
+console.log("asking useless questions in order to make a scene");
 let favouriteColor = prompt(`Great job ${firstName} ${lastName}! One last step.
 In order to create a non pseudo-randomic seed to hash your password we need to know your favorite color`);
 
-let password = firstName + lastName + favouriteColor + 21;
+console.log("finding a randomic integer bigger than 20 an lower than 22");
+let hashNumber = 21;
+let password = firstName + lastName + favouriteColor + hashNumber;
 
+console.log("let's hope in a big donation!");
 alert(`Congratulations ${firstName} ${lastName}!
 Our advanced AI algorithms have finally created a hyper complex and absolutely secure password.
 
